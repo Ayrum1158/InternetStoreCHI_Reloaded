eae3d81366fceae31de8817d3a5c59bde3837e3b
Oleksandr Korotetskyi <o.korotetskyi@chisw.com>
2021-08-04 15:29:21 +0300

    Changed default data seeding date to hardcoded value Added related migration
    Signed-off-by: Oleksandr Korotetskyi <o.korotetskyi@chisw.com>

--- InternetStoreCHIReloaded/DAL/Extensions/ModelBuilderExtentions.cs
+++ InternetStoreCHIReloaded/DAL/Extensions/ModelBuilderExtentions.cs
@@ -10,7 +10,7 @@ namespace DAL.Extensions
     {
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            var timeNow = DateTime.UtcNow;
+            var defaultCreationTime = DateTime.Parse("04/08/2021 12:20:19");
 
             modelBuilder.Entity<CategoryEntity>().HasData(
                 new CategoryEntity()
@@ -18,24 +18,24 @@ namespace DAL.Extensions
                     Id = 1,
                     Name = "Smatrphone",
                     Description = "Like the phone, but cooler",
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime
                 },
                 new CategoryEntity()
                 {
                     Id = 2,
                     Name = "TV",
                     Description = "Like the smartphone, but bigger",
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime
                 },
                 new CategoryEntity()
                 {
                     Id = 3,
                     Name = "Car",
                     Description = "Totally not like a TV, gives you a nice ride though",
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime
                 });
 
             modelBuilder.Entity<ProductEntity>().HasData(
@@ -45,8 +45,8 @@ namespace DAL.Extensions
                     Name = "Shwamsung Galaxy Milky Way 8 - Infinitely Blue",
                     Description = "Best smartphone for the best daddies",
                     Price = (decimal)1337.69,
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow,
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime,
                     CategoryId = 1
                 },
                 new ProductEntity()
@@ -55,8 +55,8 @@ namespace DAL.Extensions
                     Name = "Salami Mi LED TV 4S 50 inch",
                     Description = "4k to masses, money in cases",
                     Price = (decimal)484.74,
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow,
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime,
                     CategoryId = 2
                 },
                 new ProductEntity()
@@ -65,8 +65,8 @@ namespace DAL.Extensions
                     Name = "Edison Model S",
                     Description = "DC Powah!",
                     Price = (decimal)123000.0,
-                    CreatedDate = timeNow,
-                    UpdatedDate = timeNow,
+                    CreatedDate = defaultCreationTime,
+                    UpdatedDate = defaultCreationTime,
                     CategoryId = 3
                 });
         }
